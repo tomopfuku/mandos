8e9f9a75879a450c485a5bed262edfd516c1e82c
carolinetomo <cfukuchi@umich.edu>
2017-12-13 15:20:56 -0500

    cleaning

--- mandos/__init__.py
+++
@@ -1,6 +0,0 @@
-#from stratoML import *
-#from calc_bm_likelihood import *
-from tree_utils2 import *
-from tree_likelihood_calculator import * 
-from sim_fossils import *
-from branch_support import *
